05aeab5c294d2d879fbe98781023d3812fb07b02
Gyorgy Szaszko <adam.szaszko@gmail.com>
2024-09-13 17:27:26 +0200

    x

--- flake.nix
+++ flake.nix
@@ -1,31 +1,38 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  description = "Sewar: Image Quality Metrics for Python";
 
-pkgs.python3Packages.buildPythonPackage rec {
-  pname = "sewar";
-  version = "0.4.6";
-  pyproject = true;
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
-  # Specify source of the package. It can be a Git repo, a local directory, or a tarball
-  # src = https://github.com/andrewekhalel/sewar;
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      packages.default = pkgs.python3Packages.buildPythonPackage rec {
+        pname = "sewar";
+        version = "0.4.6";
 
-  src = pkgs.fetchFromGitHub {
-    owner = "andrewekhalel";
-    repo = "sewar";
-    rev = "d2fd6805e8dc812483a86ae231d8a5685dffdf38";  # Replace with the actual tag or commit hash
-    sha256 = "sha256-yV8MY1Iaw1tyxt1/Y2RYY67vjmnOYQLjPu6l2vDRbfw=";  # Replace with actual SHA256 hash
-  };
+        # Specify source of the package from GitHub
+        src = pkgs.fetchFromGitHub {
+          owner = "andrewekhalel";
+          repo = "sewar";
+          rev = "d2fd6805e8dc812483a86ae231d8a5685dffdf38";  # Actual commit hash
+          sha256 = "sha256-yV8MY1Iaw1tyxt1/Y2RYY67vjmnOYQLjPu6l2vDRbfw=";  # Replace with actual SHA256 hash
+        };
 
-  # Specify Python package dependencies
-  propagatedBuildInputs = [ 
-    pkgs.python3Packages.numpy
-    pkgs.python3Packages.scipy
-    pkgs.python3Packages.pillow
-    pkgs.python3Packages.nose
-  ];
+        # Specify Python package dependencies
+        propagatedBuildInputs = with pkgs.python3Packages; [
+          numpy
+          scipy
+          pillow
+          nose
+        ];
 
-  # Meta information about the package
-  meta = with pkgs.lib; {
-    description = "A short description of your package";
-    license = licenses.mit;  # Adjust this to your actual license
-  };
+        # Meta information about the package
+        meta = with pkgs.lib; {
+          description = "Python package for image quality metrics";
+          license = licenses.mit;
+        };
+      };
+    };
 }
